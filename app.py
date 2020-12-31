9fe1a2b3179cd7337babde414bc0f4af4cf8432e
Adam Gifford <48251873+adamgiffordphd@users.noreply.github.com>
2020-12-30 19:35:00 -0500

    updated index.html to display stock options

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, request, redirect
 
+# from flask import Flask
+# app_lulu = Flask(__name__)
+
+# @app_lulu.route('/hello_page_lulu')
+# def hello_world_lulu():
+#     # this is a comment, just like in Python
+#     # note that the function name and the route argument
+#     # do not need to be the same.
+#     return 'Hello World!'
+
+# if __name__ == '__main__':
+#     app_lulu.run(debug=True)
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,3 +26,5 @@ def about():
 
 if __name__ == '__main__':
   app.run(port=33507)
+
+
