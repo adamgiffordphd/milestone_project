9925e33917f8aaaaf85aa67829bf7ec4b7a189e8
Adam Gifford <48251873+adamgiffordphd@users.noreply.github.com>
2020-12-30 22:18:24 -0500

    started building index.html format and code to interact with initial input

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
-from flask import Flask, render_template, request, redirect
-
-# from flask import Flask
-# app_lulu = Flask(__name__)
-
-# @app_lulu.route('/hello_page_lulu')
-# def hello_world_lulu():
-#     # this is a comment, just like in Python
-#     # note that the function name and the route argument
-#     # do not need to be the same.
-#     return 'Hello World!'
-
-# if __name__ == '__main__':
-#     app_lulu.run(debug=True)
-
+from flask import Flask, render_template, request, redirect, url_for
+# from flask import (
+#     Blueprint, flash, g , session
+# )
 
 app = Flask(__name__)
+app.vars={}
 
-@app.route('/')
+@app.route('/index', methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+  if request.method == 'GET':
+    return render_template('index.html')
+  else: # request was a POST
+    app.vars['symbol'] = request.form['symbol']
+    # close          = option1
+    # adjusted close = option2
+    # open           = option3
+    # adjusted open  = option4
+    app.vars['close'] = request.form.get('option1')
+    app.vars['ajd_close'] = request.form.get('option2')
+    app.vars['open'] = request.form.get('option3')
+    app.vars['adj_open'] = request.form.get('option4')
 
-@app.route('/about')
-def about():
+    return redirect(url_for('app.makeplot'))
+
+@app.route('/makeplot', methods=['GET'])
+def makeplot():
+  # call/do whatever to get the data and make the visualizations
+  # return render_template('about.html')
+
+  return redirect(url_for('app.display'))
+
+@app.route('/display', methods=['GET','POST'])
+def display():
+  # call/do whatever to display the visualizations
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507, debug=True)
 
 
